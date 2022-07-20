39f310316386418e458e28f28185269a88134963
dudushy <eduardotalarico1337@gmail.com>
2022-07-20 13:44:35 -0300

    Update app-routing.module.ts

--- SideMenu/src/app/app-routing.module.ts
+++ SideMenu/src/app/app-routing.module.ts
@@ -4,17 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'accordion/Overview',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'details',
+    path: 'accordion/:path',
     loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
   }
+
+  //? OLD
+  // {
+  //   path: '',
+  //   redirectTo: 'folder/Inbox',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'folder/:id',
+  //   loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  // },
+  // {
+  //   path: 'details',
+  //   loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+  // }
 ];
 
 @NgModule({
