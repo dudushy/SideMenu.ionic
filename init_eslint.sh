4ef339b006763eeec9a6756ea6eb9002e6983d51
dudushy <eduardotalarico1337@gmail.com>
2022-07-20 13:37:51 -0300

    Create init_eslint.sh

---
+++ init_eslint.sh
@@ -0,0 +1,12 @@
+echo "- [init_eslint.sh] STARTED -"
+echo "---------------------"
+
+cd ./SideMenu
+echo ""
+echo "cd ./SideMenu"
+echo ""
+
+npm init @eslint/config
+
+echo "- [init_eslint.sh] FINISHED -"
+echo "----------------------"
