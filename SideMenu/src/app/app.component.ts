2881bdc9f27d5fa3f0a28cdcb2cc2a24f1ed2f99
dudushy <eduardotalarico1337@gmail.com>
2022-07-20 13:53:02 -0300

    Update app.component.ts

--- SideMenu/src/app/app.component.ts
+++ SideMenu/src/app/app.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController, NavController } from '@ionic/angular';
+import menuData from '../assets/menu.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  menu = menuData;
+
+  constructor(
+    private router: Router,
+    private menuCtrl: MenuController,
+    private navCtrl: NavController
+  ) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+  open(path) {}
+
+  //? OLD
+  // public appPages = [
+  //   { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+  //   { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+  //   { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+  //   { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+  //   { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+  //   { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  // ];
+  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  // constructor() {}
 }
